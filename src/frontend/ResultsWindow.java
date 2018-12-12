7ef47209e95a711fdabc9d71c4c100cef2ad7fc8
Alexander <alexporrello@gmail.com>
2018-12-12 13:48:03 -0600

    Removed outdated code.

--- src/frontend/ResultsWindow.java
+++ src/frontend/ResultsWindow.java
@@ -1,27 +1,14 @@
 package frontend;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 import backend.FileManager;
 import backend.Match;
-import jm.JMButton;
 import jm.JMColor;
-import jm.JMFocusablePanel;
 import jm.JMPanel;
-import jm.JMTextArea;
 
 public class ResultsWindow extends JMPanel {
 	private static final long serialVersionUID = -6023620346742595500L;
@@ -72,96 +59,6 @@ public class ResultsWindow extends JMPanel {
 
 				y = match.addToJMPanel(this, y);
 			}
-		}
-	}
-
-	public class MatchUI extends JMPanel {
-		private static final long serialVersionUID = 1335439437663300422L;
-
-		Match match;
-
-		JMTextArea matchLabel;
-
-		JMTextField matchNum;
-
-		JPanel deviations;
-
-		JMButton delete = new JMButton(" X ");
-
-		public MatchUI(Match match) {
-			this.match = match;
-
-			this.matchLabel = new JMTextArea(match.searchedOn.replaceAll("e#g#", "e.g.").trim());
-			this.matchNum   = new JMTextField(match.numMatches + "");
-
-			this.matchLabel.setEditable(false);
-			this.matchLabel.setFont(new JLabel().getFont());
-
-			this.matchNum.setEditable(false);
-			this.matchNum.setMinimumSize(new Dimension(15, 10));
-
-			setLayout(new BorderLayout());
-
-			buildUI();
-		}
-
-		public void buildUI() {
-			add(matchLabel, BorderLayout.CENTER);
-			add(matchNum,   BorderLayout.WEST);
-			add(delete,     BorderLayout.EAST);
-
-			try {
-				deviations = makeDeviationsPanel();
-				add(deviations, BorderLayout.SOUTH);
-			} catch (Exception e) { }
-
-			matchLabel.addMouseListener(new MouseAdapter() {				
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					if(e.getClickCount() == 1) {
-						Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-						clipboard.setContents(new StringSelection(match.searchedOn), null);
-					} else {
-						if(deviations != null) {
-							deviations.setVisible(!deviations.isVisible());
-						} 
-					}
-				}
-			});
-		}
-
-		public JPanel makeDeviationsPanel() throws Exception {
-			if(match.matches.size() >= 1) {
-				JMFocusablePanel deviationsPanel = new JMFocusablePanel();
-				deviationsPanel.setLayout(new GridBagLayout());
-
-				int y = 0;
-
-				for(String m : match.matches) {
-					if(!m.trim().toLowerCase().equals(match.searchedOn.replaceAll("e#g#", "e.g.").trim().toLowerCase())) {
-						JLabel deviation = new JLabel(m);
-						deviation.addMouseListener(new MouseAdapter() {							
-							@Override
-							public void mouseClicked(MouseEvent e) {
-								if(e.getClickCount() == 2) {
-									Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-									clipboard.setContents(new StringSelection(m), null);
-								}
-							}
-						});
-						
-						deviationsPanel.add(deviation, new GridBagConstraints(0, y++, 1, 1, 1.0, 0.0, 
-								GridBagConstraints.NORTHEAST, GridBagConstraints.HORIZONTAL, 
-								new Insets(2, 0, 0, 2), 0, 0));
-					}
-				}
-
-				if(y != 0) {
-					return deviationsPanel;
-				}
-			}
-
-			throw new Exception("No panel necessary, m8!");
 		}
 	}
 }
